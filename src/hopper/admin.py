12474ffb9d6ef07150fe3cd3448fec7ec4454edf
Michael Kaye <michaelkaye@users.noreply.github.com>
2017-02-07 19:06:46 +0000

    Make the admin site more useful with search and filtering for events

--- src/hopper/admin.py
+++ src/hopper/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from hopper.models import Event, Room, Track
 
+
+class EventAdmin(admin.ModelAdmin):
+    fieldsets = (
+            (None, {
+                'fields': ('title', 'track', 'runners', 'guidebook_desc', 'requirements', 'complete', 'public')
+            }),
+            ('Time and Room', {
+                'classes': ('collapse',),
+                'fields': ('start', 'end', 'resourceId')
+            })
+        )
+    list_filter = ('track', 'resourceId', 'public', 'complete')
+    search_fields = ['title', 'runners', 'track__title', 'resourceId__title']
+
+
 admin.site.register(Track)
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(Room)
