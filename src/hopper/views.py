8275dcdaeb6fc8e1f03a2fd9696a763badcaf422
Michael Kaye <michaelkaye@users.noreply.github.com>
2017-03-19 12:16:41 +0000

    cheeky conbook export

--- src/hopper/views.py
+++ src/hopper/views.py
@@ -13,6 +13,32 @@ logger = logging.getLogger(__name__)
 def health(request):
     return HttpResponse('')
 
+def xml(request):
+    template = loader.get_template('hopper/xml')
+    if request.user.is_authenticated:
+        queryset = Event.objects.all()
+        queryset = queryset.filter(complete=True)
+        queryset = queryset.filter(public=True)
+        logger.info("Rendering queryset {}".format(queryset))
+        # annoyingly we can't do it with a template.
+        string = "<?xml version="1.0" encoding="UTF-8" standalone="yes"?><events><days><day-1>"
+        for event in queryset:
+            string = string + _eventfragment(event)
+        string = string + "</day-1></days></events>"
+        return HttpResponse(string, content_type='text/plain')
+    else:
+        return None
+
+def _eventfragment(event):
+    title = event.title
+    abstract = event.guidebook_desc
+    persons = event.runners
+    start = event.start.format("%a at %H:%M")
+    end = event.end.format("%H:%M")
+    time = "{} to {}".format(start, end)
+    room = event.resourceId.title
+    return "<event><title>{}</title><timedate>{}</timedate><abstract>{}</abstract><persons>{}</persons><room>{}</room></event>".format(title, time, abstract, persons, room)
+
 def sched(request):
     template = loader.get_template('hopper/sched')
     if request.user.is_authenticated:
@@ -24,7 +50,7 @@ def sched(request):
         }
         logger.info("Rendering queryset {}".format(queryset))
         return HttpResponse(template.render(context, request),
-                content_type='text/csv')
+                content_type='application/xml')
     else:
         return None
 
