d14b7dc404209c79b667846c5ec61ea0c0717480
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2018-04-07 23:15:32 +0100

    imm

--- src/hopper/views.py
+++ src/hopper/views.py
@@ -59,8 +59,8 @@ def xml(request):
     return HttpResponse(string, content_type='text/plain')
 def _eventfragment(event):
     title = escape(event.title)
-    if ('\n---\n' in event.desc):
-        parts = event.desc.split('\n---\n',2);
+    if ("\n---\n" in event.desc):
+        parts = event.desc.split("\n---\n",2);
         abstract = '<abstract>'+escape(parts[0])+'</abstract>\u2029<nonsense>'+escape(parts[1])+'</nonsense>';
     else:
         abstract = '<abstract>'+escape(event.desc)+'</abstract>';
